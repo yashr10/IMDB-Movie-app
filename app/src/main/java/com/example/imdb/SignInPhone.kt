89ad3edbd417df49ee9378b10f26beeb0e1900c6
yashr10 <yashr10102@gmail.com>
2021-09-07 22:33:06 +0530

    yo

---
+++ app/src/main/java/com/example/imdb/SignInPhone.kt
@@ -0,0 +1,122 @@
+package com.example.imdb
+
+import android.R.attr.phoneNumber
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.imdb.databinding.ActivitySignInPhoneBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding :ActivitySignInPhoneBinding
+private lateinit var mAuth: FirebaseAuth
+
+class SignInPhone : AppCompatActivity() {
+
+    private var verificationId : String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInPhoneBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mAuth = FirebaseAuth.getInstance()
+
+
+        binding.progressBarPhone.visibility = View.INVISIBLE
+
+        binding.verifyOtpPoneActivity.setOnClickListener(){
+            binding.ccp.registerCarrierNumberEditText(binding.editTextPhone)
+            val checkPhoneNumber = binding.editTextPhone.text.toString().trim()
+            val phone = binding.ccp.fullNumberWithPlus
+
+
+            if (checkPhoneNumber.isEmpty() || checkPhoneNumber.length != 11){
+                binding.editTextPhone.error = "Enter a Valid Phone Number"
+                binding.editTextPhone.requestFocus()
+                Log.d("Number", checkPhoneNumber)
+                return@setOnClickListener
+            }
+
+            val options = PhoneAuthOptions.newBuilder(mAuth)
+                .setPhoneNumber(phone)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(phoneAuthCallBack)
+                .build()
+            PhoneAuthProvider.verifyPhoneNumber(options)
+
+        }
+
+        binding.codeverify.setOnClickListener(){
+            val code = binding.codePhoneActivity.text.toString().trim()
+
+            if (code.isEmpty()){
+                binding.codePhoneActivity.error = "Code Required"
+                binding.codePhoneActivity.requestFocus()
+                return@setOnClickListener
+            }
+
+            verificationId?.let {
+
+                val credential = PhoneAuthProvider.getCredential(it,code)
+                addPhoneNumber(credential)
+            }
+
+        }
+
+    }
+
+   private val phoneAuthCallBack = object  : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+
+            credential.let {
+                addPhoneNumber(it)
+            }
+            Toast.makeText(this@SignInPhone, "onVerification completed", Toast.LENGTH_SHORT).show()
+
+        }
+
+        override fun onVerificationFailed(exception: FirebaseException) {
+            Toast.makeText(this@SignInPhone, exception.message, Toast.LENGTH_LONG).show()
+            exception.message?.let { Log.d("aaaaaaaa", it) }
+        }
+
+       override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
+         //  Toast.makeText(this, "code sent", Toast.LENGTH_SHORT).show()
+            super.onCodeSent(verificationId, token)
+           this@SignInPhone.verificationId = verificationId
+
+       }
+
+
+    }
+
+    private fun addPhoneNumber(it: PhoneAuthCredential) {
+
+        mAuth.signInWithCredential (it)
+            ?.addOnCompleteListener {
+
+                if (it.isSuccessful){
+                    val intent  = Intent(this ,MainActivity::class.java )
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+                    val a = it.exception?.message
+                    if (a != null) {
+                        Log.d("ss",a)
+                    }
+                }
+            }
+
+    }
+}
---
+++ app/src/main/java/com/example/imdb/SignUpActivity.kt
@@ -0,0 +1,70 @@
+package com.example.imdb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.imdb.databinding.ActivitySignUpBinding
+import com.example.imdb.ui.dashboard.DashboardFragment
+import com.google.firebase.auth.FirebaseAuth
+
+private lateinit var binding : ActivitySignUpBinding
+private lateinit var mAuth: FirebaseAuth
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        mAuth = FirebaseAuth.getInstance()
+        setContentView(binding.root)
+
+        binding.signupSignupActivity.setOnClickListener(){
+
+            val email = binding.emailSignUpActivity.text.toString().trim()
+            val password = binding.passwordSignUpActivity.text.toString().trim()
+
+            if (email.isEmpty()){
+                binding.emailSignUpActivity.error = "Email Required"
+                binding.emailSignUpActivity.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.emailSignUpActivity.error = "Valid Email Required"
+                binding.emailSignUpActivity.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty() || password.length < 6){
+                binding.passwordSignUpActivity.error = "Minimum 6 character password required"
+                binding.passwordSignUpActivity.requestFocus()
+                return@setOnClickListener
+            }
+            registerUser(email, password)
+
+        }
+
+
+    }
+
+    private fun registerUser(email: String, password: String) {
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this) {
+
+                if (it.isSuccessful){
+                    val dashboardIntent = Intent(this, MainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    startActivity(dashboardIntent)
+                }
+                else {
+                    Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+
+    }
+}
